1d440515948d72c9a346fec211921b31bf02c808
RudychikFedorV <rudychik.fedor@gmail.com>
2023-12-31 19:43:19 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+  <router-Link to="/">Home</router-Link>
+  <router-Link to="/about">About</router-Link>
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* #app {
+  
+} */
 </style>
---
+++ src/views/AboutPage.vue
@@ -0,0 +1,16 @@
+
+import { resolveDirective } from 'vue';
+<template>
+    <h1>About component</h1>
+</template>
+
+<script>
+export default {
+    nane: 'AboutPage'
+}
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
