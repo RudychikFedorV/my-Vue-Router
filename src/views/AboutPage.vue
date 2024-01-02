2b98c0e6b6a09bf64d7132111a9e503b0bee889f
RudychikFedorV <rudychik.fedor@gmail.com>
2024-01-02 19:14:11 +0200

    commit

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,15 +1,37 @@
 <template>
-  <h1>About component</h1>
+  <div>
+    <button @click="addLike">likes</button>
+    <button @click="addDislike">dislikes</button>
+  </div>
+  <div>
+    Количество лайков <strong> {{ likes }}</strong>
+  </div>
+  <div>
+    Количество дизлайков <strong> {{ dislikes }}</strong>
+  </div>
 </template>
 
 <script>
 export default {
-  nane: "AboutPage",
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    };
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 h1 {
-  color:$red;
+  color: $red;
 }
 </style>
--- src/views/ContactsPage.vue
+++ src/views/ContactsPage.vue
@@ -1,9 +1,31 @@
 <template>
-    <h1>Contacts component</h1>
+  <div>
+    <button @click="addLike">likes</button>
+    <button @click="addDislike">dislikes</button>
+  </div>
+  <div>
+    Количество лайков <strong> {{ likes }}</strong>
+  </div>
+  <div>
+    Количество дизлайков <strong> {{ dislikes }}</strong>
+  </div>
 </template>
 
 <script>
 export default {
-    nane: 'ContactsPage'
-}
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    };
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    },
+  },
+};
 </script>
